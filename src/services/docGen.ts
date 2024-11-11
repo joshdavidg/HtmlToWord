2453b18979f0c19d54fa82501e26721af4b07a66
Josh <josh@respec.com>
2024-11-11 11:42:59 -0700

    feat (html parser): add parsing for ul, ol, a, and headings

--- src/services/docGen.ts
+++ src/services/docGen.ts
@@ -1,4 +1,4 @@
-import { Paragraph, TextRun } from "docx";
+import { ExternalHyperlink, HeadingLevel, Paragraph, TextRun } from "docx";
 import { htmlStringToElementList, recurseElements } from "../parsers/parseHtml";
 import { parseParagraphStyles, parseInnerTagStyles } from "../parsers/parseStyles";
 
@@ -7,18 +7,96 @@ export const htmlToWord = (htmlStr: string): Paragraph[] => {
     const htmlElements: Element[] = htmlStringToElementList(htmlStr);
 
     htmlElements.forEach((node: Element) => {
-        if (["p"].includes(node.nodeName.toLocaleLowerCase())) {
-            sections.push(
-                new Paragraph({
-                    children: getParagraphChildren(node),
-                    spacing: {after: 0},
-                    ...parseParagraphStyles(node.getAttribute("style")),
-                })
-            );
+        switch(node.nodeName.toLocaleLowerCase()) {
+            case "p":
+                sections.push(
+                    new Paragraph({
+                        children: getParagraphChildren(node),
+                        spacing: {after: 0},
+                        ...parseParagraphStyles(node.getAttribute("style")),
+                    })
+                );
+            break;
+            case "h1": 
+                sections.push(
+                    new Paragraph({
+                        text: node.textContent,
+                        spacing: {after: 0},
+                        heading: HeadingLevel.HEADING_1,
+                        ...parseParagraphStyles(node.getAttribute("style")),
+                    })
+                );
+            break;
+            case "h2": 
+                sections.push(
+                    new Paragraph({
+                        text: node.textContent,
+                        spacing: {after: 0},
+                        heading: HeadingLevel.HEADING_2,
+                        ...parseParagraphStyles(node.getAttribute("style")),
+                    })
+                );
+            break;
+            case "h3":
+                sections.push(
+                    new Paragraph({
+                        text: node.textContent,
+                        spacing: {after: 0},
+                        heading: HeadingLevel.HEADING_3,
+                        ...parseParagraphStyles(node.getAttribute("style")),
+                    })
+                );
+            break;
+            case "ul":
+                sections.push(...createList(node));
+            break;
+            case "ol":
+                sections.push(...createList(node));
+            break;
+            case "a":
+                sections.push(
+                    new Paragraph({
+                        children: [
+                            new ExternalHyperlink({
+                                children: [new TextRun({ text: node.textContent, style: "Hyperlink:" })],
+                                link: node.getAttribute("href")
+                            }),
+                        ],
+                    }),
+                );
+            break;
         }
     });
 
     return sections;
+}
+
+export const createList = (lNode: Element, level: number = 0): Paragraph[] => {
+    let listItems: Paragraph[] = [];
+
+    Array.from(lNode.children).forEach((elm: Element) => { 
+        if (elm.nodeName === "ul" || elm.nodeName === "ol") {
+            listItems.push(...createList(elm, level + 1));
+        } else if (elm.nodeName === "li") {
+            if (lNode.nodeName === "ul") {
+                listItems.push(
+                    new Paragraph({
+                        spacing: {after: 0},
+                        bullet: { level: level },
+                        children: getParagraphChildren(elm)
+                    })
+                );
+            } else {
+                listItems.push(
+                    new Paragraph({
+                        spacing: {after: 0, before: 12 * level},
+                        children: getParagraphChildren(elm),
+                    })
+                );
+            }
+        }
+    });
+    return listItems;
 }
 
 export const getParagraphChildren = (pNode: Element): TextRun[] => {
