a359d112bdb844c2e46013bf4243fb43433009e4
Josh <josh@respec.com>
2025-07-02 14:10:34 -0600

    refactor: add changes to integrate with API framework
    Keith Weber made the changes to the main index file to allow for
    integration with his API Framework

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import { handleErrors, routeNotFoundHandler } from "./middleware";
 const app = express();
 const PORT = 1234
 
-app.use(cors()).use(express.json({ limit: '100mb' }));
+app.use(cors());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true }));
 
+//setup routes
 app.use(wordPatcherRouter);
 app.use(healthCheckRouter);
 
+//setup middleware
 app.use(routeNotFoundHandler);
 app.use(handleErrors);
 
